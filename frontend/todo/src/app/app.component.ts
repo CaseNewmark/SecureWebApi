5c93c85d4c1fdf4997d2e234658006f078f5d571
Daniel Waldorf <daniel@woodvillage.de>
2024-12-31 10:12:30 +0100

    [impl] now with a logout button and text if no todos are there.

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -2,23 +2,28 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeartbeatClientService } from './services/heartbeat-client.service';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'todo';
 
-  constructor(private heartbeatService: HeartbeatClientService) { }
+  constructor(
+    private heartbeatService: HeartbeatClientService,
+    public authenticationService: KeycloakService
+  ) {}
 
   ngOnInit(): void {
     this.heartbeatService.connect().subscribe();
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
   }
 }
