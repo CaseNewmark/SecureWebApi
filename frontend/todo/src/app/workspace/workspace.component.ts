f482258c21ee3e25eb891128fa6eb2d26103506f
Daniel Waldorf <daniel@woodvillage.de>
2025-01-07 15:46:59 +0100

    [impl] full screen workspace

--- frontend/todo/src/app/workspace/workspace.component.ts
+++ frontend/todo/src/app/workspace/workspace.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import interact from 'interactjs';
 
 interface Box {
-  id: number;
+  id: string;
   title: string;
   content: string;
   x: number;
@@ -11,8 +12,10 @@ interface Box {
 
 @Component({
   selector: 'app-workspace',
+  standalone: true,
   templateUrl: './workspace.component.html',
-  styleUrls: ['./workspace.component.css']
+  styleUrls: ['./workspace.component.css'],
+  imports: [ CommonModule ]
 })
 export class WorkspaceComponent implements OnInit {
   boxes: Box[] = [];
@@ -22,6 +25,9 @@ export class WorkspaceComponent implements OnInit {
     interact('.draggable')
       .draggable({
         listeners: {
+          start(event) {
+            event.target.classList.add('no-select');
+          },
           move(event) {
             const target = event.target;
             const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
@@ -30,18 +36,22 @@ export class WorkspaceComponent implements OnInit {
             target.style.transform = `translate(${x}px, ${y}px)`;
             target.setAttribute('data-x', x);
             target.setAttribute('data-y', y);
+          },
+          end(event) {
+            event.target.classList.remove('no-select');
           }
         }
       });
   }
 
   addBox(): void {
+    let newId = 'box' + this.nextId++;
     this.boxes.push({
-      id: this.nextId++,
+      id: newId,
       title: 'New Box',
       content: 'Content here...',
-      x: 0,
-      y: 0
+      x: 300,
+      y: 100
     });
   }
 }
